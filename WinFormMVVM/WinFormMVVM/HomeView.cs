4b9533f5a0a5289e9875c74eca7c0ed59849c8b5
Eugeny <evgz@bk.ru>
2018-08-17 22:19:01 +0300

    Add files via upload

---
+++ WinFormMVVM/WinFormMVVM/HomeView.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+using ReactiveUI;
+using WinFormMVVM.ViewModels;
+
+namespace WinFormMVVM
+{
+    public partial class HomeView : Form, IViewFor<HomeViewModel>
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+            VM = new HomeViewModel();
+
+            // Bind the view to the ReactiveUI viewmodel
+            this.Bind(VM, x => x.EnteredText, x => x.textBoxMyInput.Text);
+            this.Bind(VM, x => x.Status, x => x.toolStripStatusLabel1.Text);
+            this.BindCommand(VM, x => x.OKCmd, x => x.btnOK);
+        }
+        public HomeViewModel VM { get; set; }
+
+        object IViewFor.ViewModel
+        {
+            get { return VM; }
+            set { VM = (HomeViewModel)value; }
+        }
+
+        HomeViewModel IViewFor<HomeViewModel>.ViewModel
+        {
+            get { return VM; }
+            set { VM = value; }
+        }
+    }
+}
---
+++ WinFormMVVM/WinFormMVVM/ViewModels/HomeViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using ReactiveUI;
+
+namespace WinFormMVVM.ViewModels
+{
+    /// <summary>
+    ///
+    /// ViewModel that will process data and events
+    /// The ViewModel inherits from the ReactiveObject class. 
+    /// This class  grants your classes access to the RaiseAndSetIfChanged helper function. 
+    /// This conveniently wires up your properties to the INotifyPropertyChanged/IObservable 
+    /// interfaces, so in one step you have a class that can be data bound AND observed using 
+    /// Reactive Extensions.
+    /// 
+    /// In the constructor for the ViewModel we set up the conditions under which something 
+    /// happens, i.e. when the OK button is clicked. An IObservable object is returned from 
+    /// the WhenAny extension, which allows you to observe whenever one or more properties 
+    /// on an object have changed. We then Subscribe to the events of the ReactiveCommand, 
+    /// which will update the status message. Note that the ViewModel has no references to 
+    /// any UI elements, which allows us to perform testing without worrying about the 
+    /// particular UI technology used.
+    /// 
+    /// (From http://www.codeproject.com/Articles/801986/Using-ReactiveUI-for-WinForms-MVVM-Design#xx4876432xx
+    ///  written by http://www.codeproject.com/Members/User-10914367)
+    /// </summary>
+    public class HomeViewModel : ReactiveUI.ReactiveObject
+    {
+        public HomeViewModel()
+        {
+            //Define conditions
+            var okCmdObs = this.WhenAny(vm => vm.EnteredText,
+            s => !string.IsNullOrWhiteSpace(s.Value));
+
+            //Create conditions
+            OKCmd = ReactiveCommand.Create(okCmdObs);
+
+            //Update Status
+            OKCmd.Subscribe(_ => Status = EnteredText + " is saved.");
+        }
+
+        string ModelString;
+        public string EnteredText
+        {
+            get { return ModelString; }
+            set { this.RaiseAndSetIfChanged(ref ModelString, value); }
+        }
+
+        string statusString = "";
+        public string Status
+        {
+            get { return statusString; }
+            set { this.RaiseAndSetIfChanged(ref statusString, value); }
+        }
+
+        public ReactiveCommand<object> OKCmd { get; private set; }
+
+
+    }
+}
